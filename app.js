61650de9427693ad119312fc1086a590ca5f30a4
alubo23 <alvinl@uninorte.edu.co>
2023-09-28 11:06:56 -0500

    Routing, servicios y ajustes generales

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Configurar el directorio de archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas a las vistas
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'templates', 'index.html'));
+});
+
+app.get('/servicios', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'templates','servicios.html'));
+});
+
+app.get('/sobre-nosotros', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'templates' ,'sobrenosotros.html'));
+});
+
+//Rutas a los contactos de los colaboradores
+app.get('/kdecastro', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'templates','kdecastro.html'));
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Servidor Express en ejecución en el puerto ${port}`);
+});
---
+++ public/js/animations.js
@@ -0,0 +1,91 @@
+//Navbar
+// element selector
+const navButton = document.querySelector(".nav-button");
+const menuContainer = document.querySelector(".menu-container");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+// variable to toggle menuContainer
+let showMenu = false;
+
+// keep svg icon inside variables
+const menuIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"/><line x1="3" y1="6" x2="21" y2="6"/><line x1="3" y1="18" x2="21" y2="18"/></svg>`;
+const closeIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/></svg>`;
+
+// add menuIcon by deafult
+navButton.innerHTML = menuIcon;
+
+// functions
+const toggleShowMenu = () => {
+  showMenu = !showMenu;
+
+  if (showMenu) {
+    navButton.innerHTML = closeIcon;
+    menuContainer.classList.add("show");
+  } else {
+    navButton.innerHTML = menuIcon;
+    menuContainer.classList.remove("show");
+  }
+};
+
+// event listener
+navButton.addEventListener("click", toggleShowMenu);
+
+menuLinks.forEach((menuLink) =>
+  menuLink.addEventListener("click", toggleShowMenu)
+);
+  
+//Scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+            sec.classList.add('show-animate');
+        } else {
+            sec.classList.remove('show-animate');
+        }
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+
+//Accordion
+(function(){
+    const titleQuestions = [...document.querySelectorAll('.trainee__title')];
+    console.log(titleQuestions)
+
+    titleQuestions.forEach(question =>{
+        question.addEventListener('click', ()=>{
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding = question.parentElement.parentElement;
+
+            addPadding.classList.toggle('trainee__padding--add');
+            question.children[0].classList.toggle('trainee__arrow--rotate');
+
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+            }
+
+            answer.style.height = `${height}px`;
+        });
+    });
+})();
