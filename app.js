09a70d00764071e651c5c6fdfaec023382b7943e
alubo23 <alvinl@uninorte.edu.co>
2023-11-15 09:18:30 -0500

    .

--- app.js
+++ app.js
@@ -2,31 +2,31 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3300;
 
 // Configurar el directorio de archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas a las vistas
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'templates', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
 });
 
 app.get('/servicios', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates','servicios.html'));
+    res.sendFile(path.join(__dirname, 'public', 'views','servicios.html'));
 });
 
 app.get('/sobre-nosotros', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates' ,'sobrenosotros.html'));
+    res.sendFile(path.join(__dirname, 'public', 'views' ,'sobrenosotros.html'));
 });
 
 app.get('/contacto', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'templates' ,'contacto.html'));
+  res.sendFile(path.join(__dirname, 'public', 'views' ,'contacto.html'));
 });
 
 //Rutas a los contactos de los colaboradores
 app.get('/kdecastro', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates','kdecastro.html'));
+    res.sendFile(path.join(__dirname, 'public', 'views','kdecastro.html'));
 });
 
 
